3dc61fe10484e6c7195cbe279596ef93bfaaf41e
David Gustafson <git@radicalskeptic.net>
2013-04-10 15:28:59 -0700

    added start,stop,process_inupt, add_player and tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,33 @@
 # lib/game.rb
 
 class Game
+
+  attr_accessor :player, :running
   attr_reader :map
 
   def initialize map
     @map = map
+    @running = false
   end
 
+  def add_player player
+    player.respond_to?(:move) ? @player =
+      player : (raise TypeError, "Player must be Mob class")
+  end
+
+  def process_input input
+    case input
+    when "q"
+      @running = false
+    end
+  end
+
+  def start
+    @running = true
+  end
+
+  def stop
+    @running = false
+  end
 
 end
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -4,19 +4,49 @@ require "spec_helper"
 require "game"
 
 describe Game do
-
-  context "#initialize" do
-
     let(:square_map_side) { 10 } 
     let(:map) { Map.new(square_map_side, square_map_side)  }
     let(:game) { Game.new(map) }
+    let(:player) { Mob.new(3,4,"@", game)}
 
+
+  context "#initialize" do
 
     it "should have a map as defined by interface" do
       game.map.should respond_to(:in_bounds?, :passable? )
     end
 
   end # #initialize
+
+  context "#add_player" do
+
+    it "responds with true when palyer added" do
+      game.add_player(player)
+    end
+  end
+
+  context "#process input" do
+
+    it "stops game when q is sent" do
+      game.start
+      expect { game.process_input("q") }.to change(game, :running).from(true).to(false)
+    end
+
+
+  end
+
+  context "#start and #stop" do
+
+    it "sets running to true" do
+      expect { game.start }.to change(game, :running).from(false).to(true)
+    end
+
+    it "sets running to false" do
+      game.start
+      expect { game.stop }.to change(game, :running).from(true).to(false)
+    end
+
+  end
 
 # context "control flow" do
 #  pending 
