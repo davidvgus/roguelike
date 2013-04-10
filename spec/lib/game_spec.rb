51220fda407abce9eaa2516cc28d45bb4c544aa4
David Gustafson <git@radicalskeptic.net>
2013-04-10 13:55:11 -0700

    commented out all pending specs

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -5,24 +5,28 @@ require "game"
 
 describe Game do
 
-  context "Game Constants" do
-    pending
-  end #Game constants
+# context "control flow" do
+#  pending 
+# end
+#
+# context "Game Constants" do
+#   pending
+# end #Game constants
 
-  context "#initialize" do
-    pending
-  end
+# context "#initialize" do
+#   pending
+# end
 
-  context "#draw_world" do
-    pending
-  end
+# context "#draw_world" do
+#   pending
+# end
 
-  context "#get_input" do
-    pending
-  end
+# context "#get_input" do
+#   pending
+# end
 
-  context "#update_world" do
-    pending
-  end
+# context "#update_world" do
+#   pending
+# end
 
 end
