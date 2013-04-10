d009719a61f8e3de943308e75b40dde0f9a68dd1
David Gustafson <git@radicalskeptic.net>
2013-04-10 16:42:22 -0700

    draws map and player with q for quit but no movement

--- main.rb
+++ main.rb
@@ -2,15 +2,29 @@
 
 require 'curses'
 require_relative 'lib/map'
+require_relative 'lib/game'
+require_relative 'lib/mob'
+require_relative 'lib/screen'
 
 include Curses
 
+MAP_WINDOW_HEIGHT = 10
+MAP_WINDOW_WIDTH = 10
+map = Map.new(MAP_WINDOW_HEIGHT, MAP_WINDOW_WIDTH)
+game = Game.new(map)
+game.map.load(Array.new(MAP_WINDOW_HEIGHT ) {Array.new(MAP_WINDOW_WIDTH) {0}})
+game.add_player Mob.new(4, 4, "@", game)
+game.start
+
+screen = Screen.new(MAP_WINDOW_HEIGHT + 2, MAP_WINDOW_WIDTH + 2, 0, 0, "|", "=")
 
 #main loop
-while game.running? do
-  game.draw_world
-  game.get_input(win)
+while game.running do
+  screen.draw_game(game)
+  input = screen.get_char
+  game.process_input(input)
   game.update_world
 end
 
-win.close
+game.stop
+screen.close
